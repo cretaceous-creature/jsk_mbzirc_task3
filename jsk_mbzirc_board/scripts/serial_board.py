386729e626a21dd78d0e24131dc1306ae05f3b78
cretaceous-creature <xychen@jsk.imi.i.u-tokyo.ac.jp>
2017-01-25 14:04:40 +0900

    add gripper point laser data, subscrib to the corresponding topic will get a int data of distance with [mm] accuracy.

--- jsk_mbzirc_board/scripts/serial_board.py
+++ jsk_mbzirc_board/scripts/serial_board.py
@@ -10,7 +10,7 @@ class Serial_board:
         self.T_HEADER = 'TH'
         self.T_TAIL = 'TT'
         self.Magdata = 'mag:!'
-        self.port = '/dev/ttyUSB1'
+        self.port = '/dev/ttyTHS2'
         self.baud = 115200
         self.serialtimeout = 0.01
         self.updaterate = 20    #20hz
@@ -28,6 +28,9 @@ class Serial_board:
         self._srv_magnet = rospy.Service("/serial_board/magnet_control", Magnet, self.__Magnet_Service)
         #magnets switch publisher
         self._pub_magnet = rospy.Publisher("/serial_board/magnet_feedback", Int16, queue_size = 1)
+        #laser distance publisher
+        self._pub_pointlaser = rospy.Publisher("/serial_board/point_laser", Int16, queue_size = 1)
+
 
     #delete function
     def __del__(self):
@@ -57,11 +60,18 @@ class Serial_board:
             data_in = self.ser.readall()
             if data_in:
                 rospy.loginfo(data_in)
-                #data will be like mag:!5!
+                #data will be like mag:!5!#223#
                 index = data_in.find(self.Magdata)
                 if not (index < 0):
                     index +=5
                     self._pub_magnet.publish(int(data_in[index:data_in.find('!',index)]))
+                    #now get the distance
+                    index = data_in.find('!#')
+                    index +=2
+                    distbyte = data_in[index:data_in.find('#',index)]
+                    if distbyte:
+                        rospy.loginfo(int(distbyte.strip('\0')))
+                        self._pub_pointlaser.publish(int(distbyte.strip('\0')))
             r.sleep()
 
 
