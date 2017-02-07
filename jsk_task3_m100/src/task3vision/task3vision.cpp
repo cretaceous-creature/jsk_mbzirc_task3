d7e6a50442bc7ad086c18ea4b95cfb40b07c2742
cretaceous-creature <xychen@jsk.imi.i.u-tokyo.ac.jp>
2017-02-07 15:24:15 +0900

    add filter, clustering for the vision node. We can test euclidean clustering, findcontour, houghcircle...

--- jsk_task3_m100/src/task3vision/task3vision.cpp
+++ jsk_task3_m100/src/task3vision/task3vision.cpp
@@ -63,9 +63,17 @@ private:
     //dynamic reconfigure
     dynamic_reconfigure::Server<jsk_mbzirc_tasks::FilterparamConfig> server;
     dynamic_reconfigure::Server<jsk_mbzirc_tasks::FilterparamConfig>::CallbackType f;
-    int h_max,s_max,v_max,h_min,s_min,v_min;
+    int s_max,v_max,s_min,v_min;
+    int r_off,g_off,b_off,ye_off,or_off;
+    bool debug_show;
     int cluster_thre_dist;
     int downsample_cluster_size, min_cluster_size;
+    std::vector<std::vector<cv::Point> > clusters;
+#define HSVRED 0
+#define HSVGREEN 60
+#define HSVBLUE 120
+#define HSVYELLOW 30
+#define HSVORANGE 20
     //functor
     struct EuclideanDistanceFunctor
     {
@@ -81,27 +89,19 @@ private:
    //test
     tf::TransformBroadcaster br;
 public:
-    void init()
+    void Init()
     {
-        img_sub_  = new message_filters::Subscriber<sensor_msgs::Image>(nh_,"/image_zenmus",2);
-        camera_info_sub_ = new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh_,"/dji_sdk/camera_info", 2);
-        uav_odom_sub_ = new message_filters::Subscriber<nav_msgs::Odometry>(nh_,"/dji_sdk/odometry",2);
-        sync = new message_filters::Synchronizer<MySyncPolicy>(MySyncPolicy(30), *img_sub_, *camera_info_sub_, *uav_odom_sub_);
-        sync->registerCallback(boost::bind(&task3_vision::imageCallback,this,_1,_2,_3));
+        img_sub_  = new message_filters::Subscriber<sensor_msgs::Image>(nh_,"/image_zenmus",1);
+        camera_info_sub_ = new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh_,"/dji_sdk/camera_info", 1);
+        uav_odom_sub_ = new message_filters::Subscriber<nav_msgs::Odometry>(nh_,"/dji_sdk/odometry",1);
+        sync = new message_filters::Synchronizer<MySyncPolicy>(MySyncPolicy(10), *img_sub_, *camera_info_sub_, *uav_odom_sub_);
+        sync->registerCallback(boost::bind(&task3_vision::ImageCallback,this,_1,_2,_3));
 
         obj_clus_pub_ = nh_.advertise<geometry_msgs::PoseArray>("/obj_cluster/centroid_pose",1);
-        /*
-        if(!nh_.getParam("enableGPU",GPUFLAG))
-            std::cout<<"fail to load the param enableGPU, Using CPU instead"<<std::endl;
-        else
-            std::cout<<"With GPU support flag = " << GPUFLAG<<std::endl;
-            */
 
         /*** dynamic reconfigure ***/
-        f = boost::bind(&task3_vision::dynamic_reconfigure_callback, this, _1,_2);
+        f = boost::bind(&task3_vision::DynamicReconfigureCallback, this, _1,_2);
         server.setCallback(f);
-        h_max=120; s_max=200; v_max=255;
-        h_min=0;   s_min=101; v_min=80;
         cluster_thre_dist = 18;
         downsample_cluster_size =1000;
         min_cluster_size = 10;
@@ -113,17 +113,25 @@ public:
 
     }
 
-    void imageCallback(const sensor_msgs::ImageConstPtr& img,
+    void ImageCallback(const sensor_msgs::ImageConstPtr& img,
                        const sensor_msgs::CameraInfoConstPtr& cam_info,
                        const nav_msgs::OdometryConstPtr& odom);
-    void dynamic_reconfigure_callback(jsk_mbzirc_tasks::FilterparamConfig &config, uint32_t level)
+    void DynamicReconfigureCallback(jsk_mbzirc_tasks::FilterparamConfig &config, uint32_t level)
     {
-      h_max = config.hsv_h_max; s_max = config.hsv_s_max; v_max = config.hsv_v_max;
-      h_min = config.hsv_h_min; s_min = config.hsv_s_min; v_min = config.hsv_v_min;
+      s_max = config.hsv_s_max; v_max = config.hsv_v_max;
+      s_min = config.hsv_s_min; v_min = config.hsv_v_min;
+      r_off = config.r_offset;g_off = config.g_offset;b_off = config.b_offset;
+      ye_off = config.ye_offset;or_off = config.or_offset;
+      debug_show = config.debug_show;
+
       cluster_thre_dist = config.cluster_threhold_dist;
       downsample_cluster_size = config.downsample_cluster_size;
       min_cluster_size = config.min_cluster_size;
     }
+
+    //clustering of one filtered image
+     std::vector<std::vector<cv::Point> > EuclideanCluster(
+             cv::Mat *image_input, cv::Mat3b *lbl, int n_factor, char c);
 
     // class's family...
     ~task3_vision()
@@ -131,9 +139,102 @@ public:
 };
 
 
+//find zeros function
+inline std::vector<cv::Point> DownSamplePoints(cv::Mat *img_input, int n)
+{
+    std::vector<cv::Point> pts_raw;
+    std::vector<cv::Point> pts;
+    if(cv::countNonZero(*img_input))
+    {
+        findNonZero(*img_input, pts_raw);
+        //clustered n_labels objects, however the problem is when getting near,
+        //too many points to be calculated, need to downsample...
+        //downsample the points if there are too much
+        pts.resize(pts_raw.size()/n);
+        for(long i = 0; i < pts.size(); i+=n)
+        {
+            pts[i] = pts_raw[i*n];
+        }
+    }
+    return pts;
+}
+
+//Euclideanclustering function
+std::vector<std::vector<cv::Point> > task3_vision::EuclideanCluster(
+        cv::Mat *image_input, cv::Mat3b *lbl, int n_factor, char c)
+{
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Point> pts = DownSamplePoints(image_input, n_factor);
+
+    if(pts.size())
+    {
+        std::vector<int> labels;
+        int n_labels = cv::partition(pts, labels, EuclideanDistanceFunctor(cluster_thre_dist));
+        contours.resize(n_labels);  //contours are the clusteres with points.
+        for (int i = 0; i < pts.size(); ++i)
+        {
+            contours[labels[i]].push_back(pts[i]);
+        }
+        /****
+     * labels vector gives every point a label by labels[pts[n]], we need a parameter to remove
+     * the labels that only contains small amount of points which could be considered as noise.
+    ****/
+        //check contours size and remove the clusteres with less points... also get the center...
+        for (std::vector<std::vector<cv::Point> >::iterator itr = contours.begin(); itr != contours.end(); ++itr)
+        {
+            if(itr->size() < min_cluster_size)
+            {
+                contours.erase(itr);
+                if(itr == contours.end())
+                    break;
+            }
+            else
+            {
+                //get the centroid of each contours
+            }
+        }
+        int label_size = contours.size();
+        for (int i = 0; i < label_size; i++)
+          {
+            cv::Vec3b color;
+            switch(c)
+            {
+            case 'a':
+                color = cv::Vec3b(rand() & 255, rand() & 255, rand() & 255);
+                break;
+            case 'r':
+                color = cv::Vec3b(0, 0, 255);
+                break;
+            case 'g':
+                color = cv::Vec3b(0, 255, 0);
+                break;
+            case 'b':
+                color = cv::Vec3b(255, 0, 0);
+                break;
+            case 'y':
+                color = cv::Vec3b(0, 255, 255);
+                break;
+            case 'o':
+                color = cv::Vec3b(0, 130, 255);
+                break;
+            default:
+                break;
+            }
+            for(int j = 0; j< contours[i].size(); j++)
+            {
+                (*lbl)(contours[i].at(j)) = color;
+            }
+          }
+    }
+    return contours;
+}
 
 
-void task3_vision::imageCallback(const sensor_msgs::ImageConstPtr& img,
+
+/****
+ * Image call back function
+****/
+void task3_vision::ImageCallback(const sensor_msgs::ImageConstPtr& img,
                                        const sensor_msgs::CameraInfoConstPtr& cam_info,
                                        const nav_msgs::OdometryConstPtr& odom)
 {
@@ -141,84 +242,119 @@ void task3_vision::imageCallback(const sensor_msgs::ImageConstPtr& img,
     {
         cv::Mat raw_image = cv_bridge::toCvCopy(img,"bgr8")->image;
         cv::Mat hsv_image;
-        cv::Mat hsv_filtered;
+        cv::Mat hsv_filtered_r, hsv_filtered_g, hsv_filtered_b, hsv_filtered_ye, hsv_filtered_or, hsv_filtered_all;
         /***********counting the timer********/
         std::clock_t start;
         double duration;
         start = std::clock();
 
-        /**************
-         * Considering two methods.
-         *************/
-
-
         /**********first HSI Filter**********/
+        {
         cv::cvtColor(raw_image,hsv_image,cv::COLOR_BGR2HSV);
-        cv::inRange(hsv_image, cv::Scalar(h_min, s_min, v_min, 0), cv::Scalar(h_max, s_max, v_max, 0), hsv_filtered);
-        cv::imshow("hsv_filtered",hsv_filtered);
+        cv::Mat hsv_r_down;
+        //r
+        cv::inRange(hsv_image, cv::Scalar(180+HSVRED-r_off, s_min, v_min, 0), cv::Scalar(180, s_max, v_max, 0), hsv_filtered_r);
+        cv::inRange(hsv_image, cv::Scalar(0, s_min, v_min, 0), cv::Scalar(r_off+HSVRED, s_max, v_max, 0), hsv_r_down);
+        hsv_filtered_r |=hsv_r_down;
+        //g
+        cv::inRange(hsv_image, cv::Scalar(HSVGREEN-g_off, s_min, v_min, 0), cv::Scalar(HSVGREEN+g_off, s_max, v_max, 0), hsv_filtered_g);
+        //b
+        cv::inRange(hsv_image, cv::Scalar(HSVBLUE-b_off, s_min, v_min, 0), cv::Scalar(HSVBLUE+b_off, s_max, v_max, 0), hsv_filtered_b);
+        //ye
+        cv::inRange(hsv_image, cv::Scalar(HSVYELLOW-ye_off, s_min, v_min, 0), cv::Scalar(HSVYELLOW+ye_off, s_max, v_max, 0), hsv_filtered_ye);
+        //or
+        cv::inRange(hsv_image, cv::Scalar(HSVORANGE-or_off, s_min, v_min, 0), cv::Scalar(HSVORANGE+or_off, s_max, v_max, 0), hsv_filtered_or);
+        }
+        //show image of r g b ye or
+        if(debug_show)
+        {
+            cv::imshow("red space",hsv_filtered_r);
+            cv::imshow("green space",hsv_filtered_g);
+            cv::imshow("blue space",hsv_filtered_b);
+            cv::imshow("yellow space",hsv_filtered_ye);
+            cv::imshow("orange space",hsv_filtered_or);
+        }
+        else
+        {
+            cv::destroyWindow("red space");cv::destroyWindow("green space");cv::destroyWindow("blue space");
+            cv::destroyWindow("yellow space");cv::destroyWindow("orange space");
+        }
+
         /**********Then Euclidean Clustering**********/
         //clustering..
-        std::vector<cv::Point> pts_raw;
-        findNonZero(hsv_filtered, pts_raw);
-        //clustered n_labels objects, however the problem is when getting near,
-        //too many points to be calculated, need to downsample...
-        //downsample the points if there are too much
-        int n = pts_raw.size()/downsample_cluster_size + 1; //begin from  1
-        std::vector<cv::Point> pts(pts_raw.size()/n);
-        for(long i = 0; i < pts.size(); i+=n)
-          {
-            pts[i] = pts_raw[i*n];
-          }
+        hsv_filtered_all = hsv_filtered_r | hsv_filtered_g | hsv_filtered_b |  hsv_filtered_ye |  hsv_filtered_or;
 
-        std::vector<int> labels;
+        /***
+         * Two methods:
+         * 1: combine all and do clustering
+         * 2: Every filter do clustering
+         * 3: Findcontours
+         * 4: Hough Circle Transform
+         ***/
+        int ptr_all = cv::countNonZero(hsv_filtered_r) + cv::countNonZero(hsv_filtered_g)
+                + cv::countNonZero(hsv_filtered_b) + cv::countNonZero(hsv_filtered_ye)
+                + cv::countNonZero(hsv_filtered_or);
+        //n_ds is the downsample factor
+        int n_ds = ptr_all/downsample_cluster_size + 1; //begin from  1
 
-        int n_labels = cv::partition(pts, labels, EuclideanDistanceFunctor(cluster_thre_dist));
-        std::vector<std::vector<cv::Point> > contours(n_labels);  //contours are the clusteres with points.
-        for (int i = 0; i < pts.size(); ++i)
-          {
-            contours[labels[i]].push_back(pts[i]);
-          }
-        /****
-         * labels vector gives every point a label by labels[pts[n]], we need a parameter to remove
-         * the labels that only contains small amount of points which could be considered as noise.
-        ****/
-        //check contours size and remove the clusteres with less points... also get the center...
-        for (std::vector<std::vector<cv::Point> >::iterator itr = contours.begin(); itr != contours.end(); ++itr)
-          {
-            if(itr->size() < min_cluster_size)
-              {
-                contours.erase(itr);
-                if(itr == contours.end())
-                  {
-                    break;
-                  }
-              }
-            else
-              {
-                //get the centroid of each contours
+        this->clusters.clear();
+        // Build a vector of random color, one for each class (label) and draw the labels
+        cv::Mat3b lbl(hsv_filtered_all.rows, hsv_filtered_all.cols, cv::Vec3b(0, 0, 0));
+        //method 1:
+        // this->clusters = this->EuclideanCluster(&hsv_filtered_all, n_ds);
+        //method 2:
+        {
+//                    this->clusters = this->EuclideanCluster(&hsv_filtered_r, &lbl, n_ds, 'r');
+//                    this->clusters = this->EuclideanCluster(&hsv_filtered_g, &lbl, n_ds, 'g');
+//                    this->clusters = this->EuclideanCluster(&hsv_filtered_b, &lbl, n_ds, 'b');
+//                    this->clusters = this->EuclideanCluster(&hsv_filtered_ye, &lbl, n_ds, 'y');
+//                    this->clusters = this->EuclideanCluster(&hsv_filtered_or, &lbl, n_ds, 'o');
+//                    imshow("Labels", lbl);
+        }
+        //method 3:
+        {
+//            std::vector<cv::Vec4i> hierarchy;
+//            std::vector<std::vector<cv::Point> > contours;
+//            cv::Mat canny_output;
+//            /// Detect edges using canny
+//            Canny( hsv_filtered_all, canny_output, 100, 100, 3 );
+//            cv::findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
+//            for( int i = 0; i< contours.size(); i++ )
+//            {
+//                cv::Scalar color;
+//                color = cv::Scalar(rand() & 255, rand() & 255, rand() & 255);
+//                drawContours( hsv_filtered_all, contours, i, color, 2, 8, hierarchy, 0, cv::Point() );
+//            }
+        }
+        //method 4:
+        {
 
-              }
-          }
+            std::vector<cv::Vec3f> circles;
 
-        // Build a vector of random color, one for each class (label)
-        std::vector<cv::Vec3b> colors;
-        for (int i = 0; i < n_labels; ++i)
-          {
-            colors.push_back(cv::Vec3b(rand() & 255, rand() & 255, rand() & 255));
-          }
+            /// Apply the Hough Transform to find the circles
+            cv::HoughCircles( hsv_filtered_all, circles, CV_HOUGH_GRADIENT,
+                              1, 10, 100, 30, 1, 200);
+            /// Draw the circles detected
+            for( size_t i = 0; i < circles.size(); i++ )
+            {
+                cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+                int radius = cvRound(circles[i][2]);
+                cv::Scalar color;
+                color = cv::Scalar(rand() & 255, rand() & 255, rand() & 255);
+                // circle center
+                cv::circle( hsv_filtered_all, center, 3, color, -1, 8, 0 );
+                // circle outline
+                cv::circle( hsv_filtered_all, center, radius, color, 3, 8, 0 );
+             }
 
-        // Draw the labels
-        cv::Mat3b lbl(hsv_filtered.rows, hsv_filtered.cols, cv::Vec3b(0, 0, 0));
-        for (int i = 0; i < pts.size(); ++i)
-          {
-            lbl(pts[i]) = colors[labels[i]];
-          }
+        }
 
-        imshow("Labels", lbl);
+
+        cv::imshow("all space",hsv_filtered_all);
+
+        //timer end
         duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
         std::cout<<"process_time is "<< duration << " second" <<'\n';
-        // cv::findContours();
-        // cv::partition();
 
         cv::waitKey(10);
     }
@@ -233,7 +369,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "task3_vision");
     task3_vision u_i2p;
-    u_i2p.init();
+    u_i2p.Init();
 
     ros::spin();
 
